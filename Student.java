90630aeadebba5e89efbead18e3fa67ed6eda4af
kalyani112 <nikamkalyani1997@gmail.com>
2020-02-21 16:42:26 +0530

    Reading input

--- Student.java
+++ Student.java
@@ -1,7 +1,9 @@
+import java.util.Scanner
 public class Student{
 public static void main(String args[])
 {
-  String message="";
-System.out.println(message);
+Scannner scan=new Scanner(System.in);
+String message=scan.nextLine();
+System.out.println("String is:"+message);
 }
 }
