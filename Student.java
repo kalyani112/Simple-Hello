c5b6edbfc4c90be5686eb05e5673febdc778c9e1
kalyani112 <nikamkalyani1997@gmail.com>
2020-02-21 16:49:35 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Student{
 public static void main(String args[])
 {
-Scannner scan=new Scanner(System.in);
+Scanner scan=new Scanner(System.in);
 String message=scan.nextLine();
 System.out.println("String is:"+message);
 }
